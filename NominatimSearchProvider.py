bd79d0e28757f1e9a70df068b285e09713b7db16
Daniel Sabo <DanielSabo@gmail.com>
2011-03-29 18:04:23 -0700

    Send view bounds to nominatim

--- NominatimSearchProvider.py
+++ NominatimSearchProvider.py
@@ -12,6 +12,7 @@ import urllib, json
 from osm2spatialite_SearchProvider import GeoDistanceSphere
 import mapnik
 
+# Nominatim URL format: http://wiki.openstreetmap.org/wiki/Nominatim
 class NominatimSearchProvider(NSObject):
     def init(self):
         self = super(NominatimSearchProvider, self).init()
@@ -21,7 +22,10 @@ class NominatimSearchProvider(NSObject):
         return self
     
     def doSearch(self, commands, center, viewBounds = None):
+        #FIXME: Add "in view" support
         url = "http://nominatim.openstreetmap.org/search?format=json&q=%s" % urllib.quote_plus(commands)
+        if viewBounds:
+            url += "&viewbox=%f,%f,%f,%f" % (viewBounds.minx, viewBounds.maxy,viewBounds.maxx,viewBounds.miny)
         json_result = urllib.urlopen(url)
         json_result = json.load(json_result)
         
@@ -30,6 +34,8 @@ class NominatimSearchProvider(NSObject):
         for row in json_result:
             result_point = mapnik.Coord(float(row["lon"]), float(row["lat"]))
             distance = GeoDistanceSphere(result_point.x, result_point.y, center.x, center.y)
+            #FIXME: osm_type doesn't really map to the types returned by other search providers
             result = {"type":row["osm_type"], "name":row["display_name"], "loc":[result_point.x,result_point.y], "distance":distance}
             results.append(result)
+        
         return results
