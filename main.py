8630b7e16d3197b4fd704b595f94602a9729da12
Daniel Sabo <DanielSabo@gmail.com>
2011-06-25 04:49:26 -0700

    Display an error message if there's an error when starting main.py

--- main.py
+++ main.py
@@ -13,18 +13,46 @@ import AppKit
 
 from PyObjCTools import AppHelper
 
-# import modules containing classes required to start application and load MainMenu.nib
-import SourceListDatasource
-import Maps4MacAppDelegate
-import MapWindowDelegate
-import MapView
-import MapnikRenderThread
-import SearchWindowDelegate
-import StyleMenuDelegate
-import LayersOutlineDataSource
-import LayersWindowDelegate
-import DownloadWindowDelegate
-import LoggerDelegate
+def doAbortError(title, msg):
+    def showMsg():
+        alert = AppKit.NSAlert.alloc().init()
+        alert.setMessageText_(title)
+        alert.setInformativeText_(msg) # Use this instead of informativeTextWithFormat to avoid % related errors
+        alert.setAlertStyle_(AppKit.NSCriticalAlertStyle)
+        alert.runModal()
+        AppKit.NSApplication.sharedApplication().stop_(None)
+    AppHelper.callAfter(showMsg)
+    AppKit.NSApplication.sharedApplication()
+    AppKit.NSApp.run()
 
-# pass control to AppKit
-AppHelper.runEventLoop()
+# import modules containing classes required to start application and load MainMenu.nib
+try:
+    import SourceListDatasource
+    import Maps4MacAppDelegate
+    import MapWindowDelegate
+    import MapView
+    import MapnikRenderThread
+    import SearchWindowDelegate
+    import StyleMenuDelegate
+    import LayersOutlineDataSource
+    import LayersWindowDelegate
+    import DownloadWindowDelegate
+    import LoggerDelegate
+except ImportError, e:
+    import traceback
+    traceback.print_exc()
+    
+    #msg = traceback.format_exc()
+    title = "Error Loading Modules"
+    doAbortError(title, str(e[0]))
+except:
+    import traceback
+    traceback.print_exc()
+    
+    msg = traceback.format_exc()
+    title = "Initialization Error"
+    
+    doAbortError(title, msg)
+else:
+    # pass control to AppKit
+    AppHelper.runEventLoop()
